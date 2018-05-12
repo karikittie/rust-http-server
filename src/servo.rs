16d5466640facc010e451bfa7e2aa08fddc68289
Karis Sponsler <karis.sponsler@gmail.com>
2018-05-11 17:56:14 -0700

    adding config functions

--- src/servo.rs
+++ src/servo.rs
@@ -8,6 +8,8 @@ pub mod servo {
     const PORT: &'static str = "port";
 
     static mut configs: Option<Configuration> = None;
+    static mut server_configs: Option<HashMap<&'static str, String>> = None;
+    static mut route_configs: Option<HashMap<String, CallBack>> = None;
 
     pub struct Server {
         pub host: String,
@@ -15,7 +17,7 @@ pub mod servo {
     }
 
     pub struct Routes {
-        pub route_map: HashMap<String, CallBack>,
+        pub route_map: &'static HashMap<String, CallBack>,
     }
 
     pub struct Configuration {
@@ -25,22 +27,58 @@ pub mod servo {
 
     impl Configuration {
         fn new() -> Configuration {
-            let server_configs = server_config();
-            let route_configs = route_config();
+            let server_conf = server_config();
+            let route_conf = route_config();
             let config = Configuration{
-                server: Server {host: server_configs.get(HOST).expect("No host defined").clone(), 
-                                port: server_configs.get(PORT).expect("No port defined").clone()},
-                routes: Routes {route_map: route_configs}
+                server: Server {host: server_conf.get(HOST).expect("No host defined").clone(), 
+                                port: server_conf.get(PORT).expect("No port defined").clone()},
+                routes: Routes {route_map: route_conf}
             };
             config
         }
     }
 
+    pub fn add_route(route_string: String, callback: CallBack) {
+        unsafe {
+            match &mut route_configs {
+                &mut Some(ref mut conf) => {
+                    conf.insert(route_string, callback);
+                },
+                &mut None => {
+                    let conf = HashMap::new();
+                    route_configs = Option::from(conf);
+                    add_route(route_string, callback);
+                }
+            }
+        }
+    }
+
+    pub fn set_host(host: String) {
+        set_server_attr(HOST, host);
+    }
+
+    pub fn set_port(port: String) {
+        set_server_attr(PORT, port);
+    }
+
+    fn set_server_attr(key: &'static str, value: String) {
+        match &mut server_configs {
+            &mut Some(ref mut conf) => {
+                conf.insert(key, value);
+            },
+            &mut None => {
+                let mut conf: HashMap<&'static str, String> = HashMap::new();
+                conf.insert(key, value);
+                server_configs = Option::from(conf);
+            }
+        }
+    }
+
     pub fn get_configs<'a>() -> &'a Configuration {
         unsafe {
-            match &configs {
-                &Some(ref config) => &config,
-                &None => {
+            match &mut configs {
+                &mut Some(ref config) => &config,
+                &mut None => {
                     let new_configs = Configuration::new();
                     configs = Option::from(new_configs);
                     get_configs()
@@ -50,17 +88,42 @@ pub mod servo {
     }
 
     fn server_config() -> HashMap<&'static str, String> {
-        let mut config: HashMap<&'static str, String> = HashMap::new();
-
-        config.insert(HOST, "127.0.0.1".into());
-        config.insert(PORT, "8000".into());
-
-        config
+        match &mut server_configs {
+            &mut Some(ref conf) => {
+                if !conf.contains_key(HOST) {
+                    conf.insert(HOST, String::from("127.0.0.1"));
+                }
+                if !conf.contains_key(PORT) {
+                    conf.insert(PORT, String::from("8000"));
+                }
+            },
+            &mut None => {
+                let mut config: HashMap<&'static str, String> = HashMap::new();
+                config.insert(HOST, String::from("127.0.0.1"));
+                config.insert(PORT, String::from("8000"));
+                server_configs = Option::from(config);
+            },
+        }
+        server_configs.expect("Server incorrectly configured")
     }
 
-    fn route_config() -> HashMap<String, CallBack> {
-        let mut config = HashMap::new();
+    fn route_config() -> &'static HashMap<String, CallBack> {
+        unsafe {
+            match &mut route_configs {
+                &mut Some(ref conf) => {
+                    conf
+                },
+                &mut None => {
+                    let conf: HashMap<String, CallBack> = HashMap::new();
+                    conf.insert(String::from("GET /"), default_home);
+                    route_configs = Option::from(conf);
+                    route_config()
+                },
+            }
+        }
+    }
 
-        config
+    fn default_home(request: Request) -> Response {
+        
     }
 }
