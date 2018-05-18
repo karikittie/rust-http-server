9d80344c03bcf1d7e3f80ada5eb6cb68793751bc
Alexis M <moneyboy.code@gmail.com>
2018-05-17 23:15:28 -0700

    more tests. updated functions to use struct methods.

--- src/requests.rs
+++ src/requests.rs
@@ -3,18 +3,18 @@ use std::collections::HashMap;
 // User request
 #[derive(Eq,Debug)]
 pub struct Request {
-    pub method : String,
-    pub route : String,
-    pub headers : HashMap<String, String>,
+    method : String,
+    route : String,
+    headers : HashMap<String, String>,
 }
 
 // Server response
 #[derive(Eq,Debug)]
 pub struct Response {
-    pub status : i32,
-    pub content_type : String,
-    pub body : String,
-    pub headers : Option<HashMap<String, String>>,
+    status : i32,
+    content_type : String,
+    body : String,
+    headers : Option<HashMap<String, String>>,
 }
 
 // Allows for equality comparisons between requests/responses
@@ -34,7 +34,6 @@ impl PartialEq for Response {
         && self.headers == other.headers
     }
 }
-
 
 // Methods available for Request and Response objects. Uses builder pattern.
 impl Request {
@@ -153,4 +152,106 @@ impl Response {
             self
     }
 
+}
+
+// Static route with 404 status, used as default bad request
+pub fn bad_route<'a>() -> Response {
+    let route = Response::new().with_status(404)
+                               .with_content_type("".to_string())
+                               .with_body("Unable to route request".to_string())
+                               .with_headers(None);
+    route
+}
+
+/*
+Gets the map of route Strings -> user-defined functions that take
+a Request object and return a Response object.
+*/
+fn get_route_map() -> Box<HashMap<String, fn(Request) -> Response>> {
+    let route_map : HashMap<String, fn(Request) -> Response> = HashMap::new();
+    return Box::new(route_map);
+}
+
+/*
+Takes the raw request string and transforms it into a Request object.
+*/
+pub fn get_request_obj(request : &str) -> Request {
+    let request = request.trim_left();
+    let lines = request.lines();
+    let mut i = 0;
+    let mut found_method : String = String::default();
+    let mut found_route : String = String::default();
+    let mut found_headers : HashMap<String, String> = HashMap::new();
+    for line in lines {
+        if i == 0 {
+            let first_args : Vec<&str> = line.split_whitespace().collect();
+            found_method = first_args[0].to_string();
+            found_route = first_args[1].to_string();
+            if found_route.ends_with("/") {
+                found_route.trim_right_matches("/");
+            }
+        }
+        else {
+            let pair : Vec<&str> = line.split(":").collect();
+            if pair.len() > 1 {
+                let key = pair[0];
+                let value = pair[1].trim_left();
+                found_headers.insert(key.to_string(), value.to_string());
+            }
+        }
+        i += 1;
+    }
+    let new_request = Request::new().with_method(found_method)
+                                    .with_route(found_route)
+                                    .with_headers(found_headers);
+    new_request
+}
+
+/*
+Takes a Request object and routes it via the method + ' ' + route
+to the appropriate user-defined function.
+TODO: we need to add the ability to pass along URL arguments to the
+user-defined function.
+*/
+pub fn route_request<'a>(request : Request) -> Response {
+    let route_map = get_route_map();
+    let mut route_request: String = request.get_method();
+    route_request.push(' ');
+    route_request.push_str(&request.get_route());
+    let route_function = route_map.get(&route_request);
+    match route_function {
+        Some(x) => x(request),
+        None => bad_route(),
+    }
+}
+
+/*
+Takes a Response object and turns it into a single String that
+can be converted to a byte-stream and written back to the user.
+*/
+pub fn stringify_response(response : Response) -> String {
+    let mut res = String::from(format!("HTTP/1.1 {}\r\ncontent-type: {}\r\n",
+                                        response.status,
+                                        response.content_type));
+    if response.headers.is_some() {
+        let headers = response.headers.as_ref().unwrap();
+        for key in headers.keys() {
+            res = res + &format!("{}: {}", key, headers[key]);
+        }
+    }
+    res = res + "\r\n";
+    res = res + &format!("{}", response.body);
+    res = res + "\r\n\r\n";
+    res
+}
+
+/*
+Takes a raw request String and transforms it into a Request object,
+routes it to a user-defined function, transforms the return from that
+function into a String and gives that back.
+*/
+pub fn get_http_response(request_string: &String) -> String {
+    let request_obj = get_request_obj(request_string);
+    let response = route_request(request_obj);
+    stringify_response(response)
 }
--- tests/req_tests.rs
+++ tests/req_tests.rs
@@ -11,10 +11,10 @@ fn request_eq_test(){
     let mut map = HashMap::new();
     map.insert("test".to_string(), "header".to_string());
 
-    let original = Request { method: method.clone(),
-                             route: route.clone(),
-                             headers: map.clone() };
-    let mut request = Request::new().with_method(method)
+    let original = Request::new().with_method(method.clone())
+                                 .with_route(route.clone())
+                                 .with_headers(map.clone());
+    let request = Request::new().with_method(method)
                                     .with_route(route)
                                     .with_headers(map);
     assert_eq!(true, original == request)
@@ -28,13 +28,32 @@ fn response_eq_test() {
     let mut map = HashMap::new();
     map.insert("test".to_string(), "header".to_string());
 
-    let original = Response { status: status,
-			                  content_type: content.clone(),
-			                  body: body.clone(),
-			                  headers: Option::from(map.clone()) };
-    let mut response = Response::new().with_status(status)
-				                      .with_content_type(content)
-				                      .with_body(body)
-				                      .with_headers(Option::from(map));
+    let original = Response::new().with_status(status)
+				                  .with_content_type(content.clone())
+				                  .with_body(body.clone())
+				                  .with_headers(Option::from(map.clone()));
+    let response = Response::new().with_status(status)
+				                  .with_content_type(content)
+				                  .with_body(body)
+				                  .with_headers(Option::from(map));
     assert_eq!(true, original == response)
+}
+
+#[test]
+fn request_neq_test() {
+    let original = Request::new();
+    let request = Request::new().with_method("method".to_string())
+                                 .with_route("route".to_string());
+
+    assert_eq!(true, original != request)
+}
+
+#[test]
+fn response_neq_test() {
+    let original = Response::new();
+    let request = Response::new().with_status(404_i32)
+                                 .with_content_type("content".to_string())
+                                 .with_body("body".to_string());
+
+    assert_eq!(true, original != request)
 }
